b01e57a67fada6067bfcf08bcb5948ed0135562c
Yoni <teamagam1@gmail.com>
2016-05-29 16:26:41 +0300

    console.error for logging errors

--- routes/images.js
+++ routes/images.js
@@ -34,7 +34,7 @@ router.post("/", upload.single('image'), function (httpRequest, httpResponse, ne
     if(error){
         //Bad Request
         error.status = 400;
-        console.log(error);
+        console.error(error);
         return next(error);
     }
 
