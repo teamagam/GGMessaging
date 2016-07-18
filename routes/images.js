2356bbad600290db434973de29239b7ca01f0334
Dor <teamagam3@gmail.com>
2016-07-18 10:30:32 +0300

    Hotfixed and refactored file upload

--- routes/images.js
+++ routes/images.js
@@ -27,74 +27,106 @@ module.exports = router;
  */
 router.post("/", upload.single('image'), function (httpRequest, httpResponse, next) {
     var file = httpRequest.file;
-
-    // If the file doesn't exist, throw an error
-    if(file == undefined || file == null) {
-        httpResponse.status(400).send({ error : 'File is required'})
-
-        return next(res);
-    }
-
-    // Get the file extension to validate the file
-    var splittedFileName = file.originalname.split(".")
-    var fileExtension = "";
-
-    if(splittedFileName.length > 1) {
-        fileExtension = splittedFileName[splittedFileName.length - 1];
-    }
-
-    // Throw an error if we can't find both mimetype and file extension
-    if(config.uploadConfig.acceptedMimeTypes.indexOf(file.mimetype) == -1 &&
-        config.uploadConfig.acceptedExtensions.indexOf(fileExtension) == -1) {
-
-        httpResponse.status(400).send({ error : 'Unsupported file'});
-
-        return next(res);
-    }
-
-    //JSON validation
     var msg = JSON.parse(httpRequest.body.message);
-    var error = common.validateMessage(msg);
+    var error = validateInput(msg, file);
 
-    if(error){
-        // Bad Request
-        error.status = 400;
-        console.error(error);
+    if(error) {
         return next(error);
     }
 
+    uploadImageToStorage(msg, file);
 
-    // Random string for multi-part upload
-    var boundaryKey = Math.random().toString(16);
-    var options = {
-        method: 'POST',
-        url: urlStorage,
-        headers: {
-            'cache-control': 'no-cache',
-            'content-type': 'multipart/form-data; boundary=---' + boundaryKey
-        },
-        formData: {
-            image: {
-                value: file.buffer,
-                options: {filename: file.originalname, contentType: null}
-            }
+    // *******************
+    //      Functions
+    // *******************
+
+    function validateFile(file) {
+        // If the file doesn't exist, throw an error
+        if (file == undefined || file == null) {
+            var err = new Error('File is requried');
+            err.status = 400;
+
+            return err;
         }
-    };
 
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
+        // Get the file extension to validate the file
+        var splittedFileName = file.originalname.split(".")
+        var fileExtension = "";
 
-        var fileStorage = JSON.parse(body);
-        var id = fileStorage._id;
-        msg.content.url = urlStorage + '/' + id;
+        if (splittedFileName.length > 1) {
+            fileExtension = splittedFileName[splittedFileName.length - 1];
+        }
 
-        console.log("file uploaded to storage with url: " + msg.content.url);
+        // Throw an error if we can't find both mimetype and file extension
+        if (config.uploadConfig.acceptedMimeTypes.indexOf(file.mimetype) == -1 &&
+            config.uploadConfig.acceptedExtensions.indexOf(fileExtension) == -1) {
 
-        common.saveNewMessageToMongoDB(msg,
-            function (err) {
-                next(err);
-            }, function (message) {
-                httpResponse.send(message);
-            });
-    });
+            var err = new Error('Unsupported file');
+            err.status = 400;
+
+            return err;
+        }
+    }
+
+    function validateHttpRequest(msg) {
+        var error = common.validateMessage(msg);
+
+        if (error) {
+            // Bad Request
+            error.status = 400;
+            console.error(error);
+
+            return error;
+        }
+    }
+
+    function validateInput(msg, file) {
+        var fileError = validateFile(file);
+        var requestError = validateHttpRequest(msg);
+
+        return fileError || requestError;
+    }
+
+    function uploadImageToStorage(msg, file) {
+        var options = createRequestOptions(file);
+
+        request(options, function (error, response, body) {
+            if (error)
+                return next(new Error(error));
+
+            var fileStorage = JSON.parse(body);
+            var id = fileStorage._id;
+
+            msg.content.url = urlStorage + '/' + id;
+
+            console.log("file uploaded to storage with url: " + msg.content.url);
+
+            common.saveNewMessageToMongoDB(msg,
+                function (err) {
+                    next(err);
+                }, function (message) {
+                    httpResponse.send(message);
+                });
+        });
+    }
+
+    function createRequestOptions(file) {
+        // Random string for multi-part upload
+        var boundaryKey = Math.random().toString(16);
+        var options = {
+            method: 'POST',
+            url: urlStorage,
+            headers: {
+                'cache-control': 'no-cache',
+                'content-type': 'multipart/form-data; boundary=---' + boundaryKey
+            },
+            formData: {
+                image: {
+                    value: file.buffer,
+                    options: {filename: file.originalname, contentType: null}
+                }
+            }
+        };
+        return options;
+    }
 });
