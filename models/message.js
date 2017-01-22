b68192e071cf20a7f9aeddf49f2b878b86f8deb3
Bar <barmako@gmail.com>
2017-01-22 10:01:34 +0200

    Added messages with type "VectorLayer" to the acceptable messages collection.

--- models/message.js
+++ models/message.js
@@ -17,7 +17,7 @@ var messageSchema = new mongoose.Schema({
     type: {
         type: String,
         enum: {
-            values: ['Text', 'Geo', 'UserLocation', 'Image', 'Sensor', 'Alert'],
+            values: ['Text', 'Geo', 'UserLocation', 'Image', 'Sensor', 'Alert', 'VectorLayer'],
             message: 'message type `{VALUE}` is not valid'
         },
         required: true
